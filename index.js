d044710bb34c35e8057addc7b9a4ebededb782f6
otienodominic <otienodominic@gmail.com>
2020-01-17 16:17:23 +0300

    Final deploy of the app

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
-const express = require('express');
-const cowsay = require('cowsay');
-const cors = require('cors');
+const express = require('express')
+const cowsay = require('cowsay')
+const cors = require('cors')
+const path = require('path')
 
-// Create server
-const app = express();
+// Create the server
+const app = express()
 
-// server the api with
-app.get('/api/cow/:say', cors(), async(req, res, next) => {
-    try{
-        const text = req.params.say
-        const moo = cowsay.say({text})
-        res.json({moo})
-    }
-    catch(err){
-        next(err)
-    }
-})
-// default route
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('/', cors(), async(req, res, next) => {
-    try {
-        res.send('Hello This is Dominic!!');
-    }
-    catch(err){
-        next(err)
-    }
+// Serve our api route /cow that returns a custom talking text cow
+app.get('/api/cow/:say', cors(), async (req, res, next) => {
+  try {
+    const text = req.params.say
+    const moo = cowsay.say({ text })
+    res.json({ moo })
+  } catch (err) {
+    next(err)
+  }
 })
 
-
-// serve our api with a custom moo
-
-app.get('/api/cow/', cors(), async(req, res, next) => {
-    try {
-        const moo = cowsay.say({text: 'Hello Dominic!'})
-        res.json({moo})
-    }
-    catch(err){
-        next(err)
-    }
+// Serve our base route that returns a Hellow World cow
+app.get('/api/cow/', cors(), async (req, res, next) => {
+  try {
+    const moo = cowsay.say({ text: 'Hello World!' })
+    res.json({ moo })
+  } catch (err) {
+    next(err)
+  }
 })
 
-// Create the server and serve
-const PORT = process.env.PORT || 3000;
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
-app.listen(PORT,cors(), () => {
-    console.log(`Server up and running on Port ${PORT} This is just great!!`)
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`)
 })
